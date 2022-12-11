1c963f6ac86c65deed4dd35a7f352f4a3f511bc0
Kmconant13 <120330716+Kmconant13@users.noreply.github.com>
2022-12-11 17:38:51 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function setup() {
+  createCanvas(400,400);
+  var Sprite = createSprite (10,10,50,50)
+}
+
+function draw() 
+{
+  background(30);
+drawSprites()
+}
+console.log ("message")
+
+
+
